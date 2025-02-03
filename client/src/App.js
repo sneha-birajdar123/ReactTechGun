9612cab39408e20de59d7ee936e4b84856c38e50
sneha <neha06844@gmail.com>
2025-02-03 12:55:52 +0530

    react

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+
+
+let dummyExpense = [
+  {
+    id: "e1",
+    title: "school fee",
+    amount: 250,
+    date: new Date(2021, 4, 12)
+  },
+  {
+    id: "e2",
+    title: "books",
+    amount: 230,
+    date: new Date(2021, 4, 2)
+  },
+  {
+    id: "e3",
+    title: "house rent",
+    amount: 800,
+    date: new Date(2024, 4, 12)
+  },
+  {
+    id: "e4",
+    title: "food",
+    amount: 540,
+    date: new Date(2022, 5, 12)
+  },
+]
+
+
 const App = () => {
-
-  let expenses = [
-    {
-      id: "e1",
-      title: "school fee",
-      amount: 250,
-      date: new Date(2021, 4, 12)
-    },
-    {
-      id: "e2",
-      title: "books",
-      amount: 230,
-      date: new Date(2021, 4, 2)
-    },
-    {
-      id: "e3",
-      title: "house rent",
-      amount: 800,
-      date: new Date(2024, 4, 12)
-    },
-    {
-      id: "e4",
-      title: "food",
-      amount: 540,
-      date: new Date(2022, 5, 12)
-    },
-  ]
-
-  const addExpenseHandler = (expenses) => {
-
+  const [expenses, setExpenses] = useState(dummyExpense)
+  const addExpenseHandler = (expense) => {
+    const updatedExpense = [expense, ...expenses]
+    setExpenses(updatedExpense)
   }
 
   return (
 
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses item={expenses}/>
-      
+      <Expenses item={expenses} />
     </div>
   )
 }
--- client/src/components/Expenses/Expenses.js
+++ client/src/components/Expenses/Expenses.js
@@ -6,22 +6,16 @@ import Card from "../UI/Card";
 const Expenses = (props) => {
     return (
         <Card className="expenses">
-            <ExpenseItems
-                date={props.item[0].date}
-                title={props.item[0].title}
-                amount={props.item[0].amount} />
-            <ExpenseItems
-                date={props.item[1].date}
-                title={props.item[1].title}
-                amount={props.item[1].amount} />
-            <ExpenseItems
-                date={props.item[2].date}
-                title={props.item[2].title}
-                amount={props.item[2].amount} />
-            <ExpenseItems
-                date={props.item[3].date}
-                title={props.item[3].title}
-                amount={props.item[3].amount} />
+            {
+                props.item.map(
+                    expense => (
+                        <ExpenseItems
+                        date={expense.date}
+                        title={expense.title}
+                        amount={expense.amount} />
+                    )
+                )
+            }
         </Card>
     )
 }
